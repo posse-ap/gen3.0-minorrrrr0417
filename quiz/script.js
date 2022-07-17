b136c198a74c5f50e7816dc284e319fa6bdc2c09
hiki-minori <ishiminono0417@icloud.com>
2022-07-17 22:00:45 +0900

    ファイルの変更

---
+++ quiz/script.js
@@ -0,0 +1,52 @@
+const quizDescription = document.getElementsByClassName('quizDescription');
+
+const quizChoice = document.getElementsByClassName('quizChoice');
+
+// const quizSet = [
+//   {q: '日本のIT人材が2030年には最大どれくらい不足すると言われているでしょうか？', c: ['A0', 'A1', 'A2']},
+//   {q: 'What is B?', c: ['B0', 'B1', 'B2']},
+//   {q: 'What is C?', c: ['C0', 'C1', 'C2']},
+// ];
+
+// {
+//   "quiz": [
+//       {
+//           "q": "日本のIT人材が2030年には最大どれくらい不足すると言われているでしょうか？",
+//           "a": [
+//               "約28万人",
+//               "約79万人",
+//               "約183万人"
+//           ],
+//           "correct": 2,
+          
+//       }
+//     ]
+//   }
+
+
+// const CORRECT_ANSWERS = [
+//   {
+//     index: 1,
+//     value: '約79万人'
+//   },
+//   {
+//     index: 2,
+//     value: 'X-TECH'
+//   },
+//   {
+//     index: 0,
+//     value: 'Internet of Things'
+//   },
+//   {
+//     index: 0,
+//     value: 'Society 5.0'
+//   },
+//   {
+//     index: 0,
+//     value: 'Web3.0'
+//   },
+//   {
+//     index: 1,
+//     value: '約5倍'
+//   }
+// ];
