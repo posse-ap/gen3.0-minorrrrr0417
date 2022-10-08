1da4da484563aa19ad3b7f0eaaee9c8d243179a7
hiki-minori <ishiminono0417@icloud.com>
2022-10-08 12:36:19 +0900

    writing完了

--- quiz.writing/script.js
+++ quiz.writing/script.js
@@ -26,10 +26,7 @@ let quiz =[
     number: 1,
     question: "日本のIT人材が2030年には最大どれくらい不足すると言われているでしょうか？",
     picture: 0,
-    option1 :"約28万人",
-    option2 :"約79万人",
-    option3 :"約183万人",
-    correctAnswer: "約79万人",
+    answerArray:['約28万人', '約79万人', '約183万人'],
     quote: "経済産業省 2019年3月 - IT 人材需給に関する調査"
   },
   
@@ -37,27 +34,21 @@ let quiz =[
     number: 2,
     question: "既存業界のビジネスと、先進的なテクノロジーを結びつけて生まれた、新しいビジネスのことをなんと言うでしょう？",
     picture: 0,
-    option1 :"INTECH",
-    option2 :"BIZZTECH",
-    option3 :"X-TECH",
+    answerArray:['INTECH', 'BIZZTECH', 'X-TECH'],
     correctAnswer: "X-TECH",
   },
   {
     number: 3,
     question: "IoTとは何の略でしょう？",
     picture: 0,
-    option1 :"Internet of Things",
-    option2 :"Integrate into Technology",
-    option3 :"Information on Tool",
+    answerArray:['Internet of Things', 'Integrate into Technology', 'Information on Tool'],
     correctAnswer: "Internet of Things",
   },
   {
     number: 4,
     question: "日本が目指すサイバー空間とフィジカル空間を高度に融合させたシステムによって開かれる未来社会のことをなんと言うでしょうか？",
     picture: 0,
-    option1 :"Society 5.0",
-    option2 :"CyPhy",
-    option3 :"SDGs",
+    answerArray:['Society 5.0', 'CyPhy', 'SDGs'],
     correctAnswer: "Society 5.0",
     quote: "Society5.0 - 科学技術政策 - 内閣府"
   },
@@ -65,79 +56,79 @@ let quiz =[
     number: 5,
     question: "イギリスのコンピューター科学者であるギャビン・ウッド氏が提唱した、ブロックチェーン技術を活用した「次世代分散型インターネット」のことをなんと言うでしょう？",
     picture: 0,
-    option1 :"Web3.0",
-    option2 :"NFT",
-    option3 :"メタバース",
+    answerArray:['Web3.0', 'NFT', 'メタバース'],
     correctAnswer: "Web3.0",
   },
   {
     number: 6,
     question: "先進テクノロジー活用企業と出遅れた企業の収益性の差はどれくらいあると言われているでしょうか？",
     picture: 0,
-    option1 :"約2倍",
-    option2 :"約5倍",
-    option3 :"約11倍",
+    answerArray:['約2倍', '約5倍', '約11倍'],
     correctAnswer: "約5倍",
     quote: "Technology Vision 2021"
   },
 ]
 
+const array = quiz.slice()
 
+  function shuffle(arr) {
+  for ( let i = 0; i < array.length ; i++) {
+    const k = array.length-1;
+    const j = Math.floor(Math.random() * k);
+    [array[j], array[k]] = [array[k], array[j]];
+  }
+  return array;
+};
+const quizArray =shuffle(quiz)
 
+const createQuizHtml = (quizItem, questionNumber) => {
+  
+  for(let i = array.length - 1; i >= 0; i--) {
+      quizItem.answerArray.forEach((option,index)=>{
+        const j = Math.floor(Math.random()*(index));
+        [quizItem.answerArray[j], quizItem.answerArray[index]] = [quizItem.answerArray[index], quizItem.answerArray[j]];
+      })
+    }
+  
+  const answerHtml = quizItem.answerArray.map((i)=>{
+    return `<li>`
+          +`<button class="quizChoice quizChoice${i}" id="option${i}-1" >${i}</button>`
+          +`<i class="iconArrow"><img src="/img/icon/icon-arrow.svg" alt=""></i>`   
+          +`</li>`
+    }).join('')
 
-
-
-
-function create_quiz(i){
-  const quiz_area = document.getElementById("quizSection");
-  const quiz_html =`<div class="quizWrapper">`
+  return `<div class="quizWrapper">`
                       +`<div class="quizContents">`                 
-                        +`<h2 class="quizNumber" id="question_number_js"> Q${i+1}</h2>`
-                        +`<h4 class="quizDescription">${quiz[i].question}</h4>`
+                        +`<h2 class="quizNumber" id="question_number_js"> Q${questionNumber+1}</h2>`
+                        +`<h4 class="quizDescription">${quizItem.question}</h4>`
                       +`</div>`
                       +`<div class ="quizPeople">`
-                        +`<img src='/img/quiz/${quiz[i].number}.png'>`
+                        +`<img src='/quiz.writing/img copy/quiz/img-quiz0${quizItem.number}.png'>`
                       +`</div>`
                       +`<div class="quizAnswer">A</div>`
-                      +`<ul class="quizAnswerWrapper">`
-                          +`<li>`
-                            +`<button class="quizChoice quizChoice${i}" id="option${i}-1" >${quiz[i].option1}`
-                            +`<i class="iconArrow"><img src="/img/icon/icon-arrow.svg" alt=""></i>`   
-                          +`</li>`
-                          +`<li>`
-                            +`<button class="quizChoice quizChoice${i}" id="option${i}-2" >${quiz[i].option2}`
-                            +`<i class="iconArrow"><img src="/img/icon/icon-arrow.svg" alt=""></i>`   
-                          +`</li>`
-                          +`<li>`
-                            +`<button class="quizChoice quizChoice${i}" id="option${i}-3">${quiz[i].option3}`
-                            +`<i class="iconArrow"><img src="/img/icon/icon-arrow.svg" alt=""></i>`   
-                          +`</li>`
-                      +`</ul>` 
+                      +`<ul class="quizAnswerWrapper">${answerHtml}</ul>` 
                       
-                    +`<div class="correctWrapper" id="correctArea${i}">`
+                    +`<div class="correctWrapper" id="correctArea${questionNumber}">`
                       +`<div class="contentsPosition">`
                         +`<div class = "correct">正解！</div>`
                         +`<div class = "answerAreaPosition">`
                           +`<div class = "correctA">A</div>`
-                          +`<div class = "correctAnswer" >${quiz[i].correctAnswer}</div>`
+                          +`<div class = "correctAnswer" >${quizItem.correctAnswer}</div>`
                         +`</div>`
                       +`</div>`
                     +`</div>`
                     
-                    +`<div class="wrongWrapper" id="wrongArea${i}">`
+                    +`<div class="wrongWrapper" id="wrongArea${questionNumber}">`
                       +`<div class="contentsPosition">`
                         +`<div class = "wrong">不正解...</div>`
                         +`<div class = "answerAreaPosition">`
                           +`<div class = "wrongA">A</div>`
-                          +`<div class = "wrongAnswer">${quiz[i].correctAnswer}</div>`
+                          +`<div class = "wrongAnswer">${quizItem.correctAnswer}</div>`
                         +`</div>`
                       +`</div>`
                     +`</div>`
-                    +`<div>${quote_area(i)}</div>`
+                    +`<div>${quote_area(questionNumber)}</div>`
                     +`</div>`;
-                    
-  return quiz_area.insertAdjacentHTML("beforeend",quiz_html);
-  
 }
 
 
@@ -148,7 +139,6 @@ function quote_area(i){
                     +`<img src="/img/icon/icon-note.svg" alt="">`
                     +`<div class="referenceArea">${quiz[i].quote}</div>`
                   +`</div>`;
-                  // +`</div>`;
     if(quiz[i].quote){
                     return quote_html;
                   }else{
@@ -158,9 +148,6 @@ function quote_area(i){
 
 
 // 正誤判定
-
-
-
 
   window.addEventListener('DOMContentLoaded', () => {
     const buttons = document.querySelectorAll(`.quizChoice`);
@@ -179,7 +166,6 @@ function quote_area(i){
           }
           setDisabled(choices);
           
-          
           button.classList.add(`is-selected`);
           
           if (button.innerText === quiz[quizNumber].correctAnswer){
@@ -187,40 +173,16 @@ function quote_area(i){
           }else{
             WrongAnswerArea.classList.add(`wrongZone`);
           }
-          
         });
       });
       
   })
   
+const quiz_area = document.getElementById("quizSection");
   
-  
+  quiz_area.innerHTML = quizArray.map((quizItem, questionNumber) => {
+    return createQuizHtml(quizItem, questionNumber)
+  }).join('');
 
-function shuffle(arr) {
-  for ( let i = 0; i < arr.length ; i++) {
-    const str1 = quiz[i].option1;
-    const str2 = quiz[i].option2;
-    const str3 = quiz[i].option3;
-    const optionArray =[str1, str2, str3]; 
-    const k = optionArray.length -1;
-    const j = Math.floor(Math.random() * k);
-    [optionArray[j], optionArray[k]] = [optionArray[k], optionArray[j]];
-    // console.log(optionArray)
-  }
-  return arr;
-};
-shuffle(quiz)
-
-  
-
-for(let i = 0; i < quiz.length; i++) {
-  const j = Math.floor(Math.random()*(i +1));
-  [quiz[j], quiz[i]] = [quiz[i], quiz[j]];
-}
-
-  for(let i = 0; i < quiz.length; i++) {
-  create_quiz(i);
-  
-  }
 
 
